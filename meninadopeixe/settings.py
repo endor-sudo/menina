94774ebe42f13124a45d2291a5dc2a9966112fa9
username <apc.freitas@sapo.pt>
2020-06-01 00:09:47 +0100

    is it now ready for heroku?

--- meninadopeixe/settings.py
+++ meninadopeixe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'id=yjmk#_hc#n#b*$#szf_@!7nhsyw7*be(c9r1=v^$plp*jp9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
