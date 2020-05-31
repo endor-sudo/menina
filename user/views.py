4a18b4827e99a42379f0ee637ef21dfde2f24327
username <apc.freitas@sapo.pt>
2020-05-31 19:46:19 +0100

    before user registration

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
 
 # Create your views here.
+def logout_view(request): 
+    """Log the user out."""
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
